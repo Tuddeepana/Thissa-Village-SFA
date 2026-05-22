cc1b91a27df209fb396c89afc15fdf67309c2816
Achintha Uddeepana <129895353+Tuddeepana@users.noreply.github.com>
2026-05-22 12:25:49 +0530

    feat:kot printer tunnel implement

--- frontend/src/lib/kotPrinter.ts
+++ frontend/src/lib/kotPrinter.ts
@@ -304,8 +304,7 @@ export const printKotSlip = async (data: KotSlipData): Promise<void> => {
     </html>
   `;
 
-    printWindow.document.write(kotHTML);
-    printWindow.document.close();
-  }
+  printWindow.document.write(kotHTML);
+  printWindow.document.close();
 };
 
